7656c89392eca7c206152418f7ba360ba07ab846
Mitsu-3284 <103481461+Mitsu-3284@users.noreply.github.com>
2022-05-16 16:01:23 +0900

    Update topsic_n.py
    topsic_n提出

--- topsic_n.py
+++ topsic_n.py
@@ -2,14 +2,19 @@
 
 N = int(input())
 A = list(map(int,input().split()))
-a = ""
+a = "YES"
 
 for x in range(N-2):
     for y in range(x+1,N-1):
         for z in range(y+1,N):
             if (A[x] + A[z] ) /2 < A[y]:
-                a = "YES"
-            else:
                 a = "NO"
+                break
+        else:
+            continue
+        break
+    else:
+        continue
+    break
 
 print(a)
