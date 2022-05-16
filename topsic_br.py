a2674dd53428ffc16b97ee014f1cedbcfda68b16
Mitsu-3284 <103481461+Mitsu-3284@users.noreply.github.com>
2022-05-16 14:29:00 +0900

    Create topsic_br.py
    topsic_br提出

---
+++ topsic_br.py
@@ -0,0 +1,15 @@
+#数列の操作
+
+N = int(input())
+A = list(map(int,input().split()))
+B = sorted(list(map(int,input().split())))
+a = 0
+
+for x in range(N):
+        if A[x] != B[x]:
+            a = a + abs(A[x] - B[x])
+
+        if A[x] == B[x]:
+            continue
+
+print(a)
