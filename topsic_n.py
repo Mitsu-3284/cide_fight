848da73536695f0da3f657f2a80df3f398ded87b
Mitsu-3284 <103481461+Mitsu-3284@users.noreply.github.com>
2022-05-17 14:05:19 +0900

    Update topsic_n.py
    topsic_n提出

--- topsic_n.py
+++ topsic_n.py
@@ -7,7 +7,7 @@ a = "YES"
 for x in range(N-2):
     for y in range(x+1,N-1):
         for z in range(y+1,N):
-            if (A[x] + A[z] ) /2 < A[y]:
+            if (A[x] + A[z] ) /2 >= A[y]:
                 a = "NO"
                 break
         else:
